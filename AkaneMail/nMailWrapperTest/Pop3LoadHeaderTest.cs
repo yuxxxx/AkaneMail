e801ed80e74476e7f40b94845f7a6cd8dc7a62f2
ゆうなんとか <yuunnxx@gmail.com>
2014-02-02 09:45:33 +0900

    'Pop3.LoadHeader' test

---
+++ AkaneMail/nMailWrapperTest/Pop3LoadHeaderTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MiniMailTest
+{
+    [TestClass]
+    public class Pop3LoadHeaderTest
+    {
+        [TestMethod]
+        public void LoadHeaderSuccess()
+        {
+            
+        }
+    }
+
+    [TestClass]
+    public class Pop3LoadHeaderFailureTest
+    {
+
+    }
+}
